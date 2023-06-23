9b74a6a9081a12490ec3a1c4674e4fd4f9f63036
Blender-vendor <ruben.bissett@gmail.com>
2023-06-24 00:14:24 +1000

    Fix: Button
    Fixed compiler error. rebuilt a puzzle.

--- Assets/scripts/Puzzle/buttonBehaviour.cs
+++ Assets/scripts/Puzzle/buttonBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonBehaviour : MonoBehaviour
+public class buttonBehaviour : MonoBehaviour
 {
 
     public bool playerInZone;
